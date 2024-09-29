d56009c132d79e29801a02f213471c35efb43117
AN-tech24 <tech.hibi141@gmail.com>
2024-09-29 15:40:16 +0900

    Update update.php

--- story_syusei/update.php
+++ story_syusei/update.php
@@ -7,6 +7,7 @@ $setting  = $_POST["setting"];
 $first_scene = $_POST["first_scene"];
 $choice1_text   = $_POST["choice1_text"];
 $choice2_text    = $_POST["choice2_text"];
+$id = $_POST["id"];
 
 //2. DB接続します
 include("funcs.php");
@@ -16,11 +17,13 @@ $pdo = db_conn();
 //３．データ登録SQL作成
 
 $stmt = $pdo->prepare("UPDATE gs_cm_table SET hero=:hero,setting=:setting,first_scene=:first_scene,choice1_text=:choice1_text,choice2_text=:choice2_text WHERE id=:id");
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->bindValue(':hero',   $hero,   PDO::PARAM_STR);
 $stmt->bindValue(':setting',  $setting,  PDO::PARAM_STR);
 $stmt->bindValue(':first_scene',    $first_scene,    PDO::PARAM_STR);  // 修正: $first_scene を使用
 $stmt->bindValue(':choice1_text', $choice1_text, PDO::PARAM_STR);
 $stmt->bindValue(':choice2_text', $choice2_text, PDO::PARAM_STR);  // 修正: $choice2_text を使用
+$status = $stmt->execute(); //実行
 
 //４．データ登録処理後
 if($status==false){
